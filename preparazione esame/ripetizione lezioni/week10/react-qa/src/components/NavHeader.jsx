2ab283cd3871ae05ed552182e1ca25c71034a280
Alessandro Scavone <58234359+alexscavo@users.noreply.github.com>
2024-08-16 13:15:07 +0200

    fine week 10

---
+++ preparazione esame/ripetizione lezioni/week10/react-qa/src/components/NavHeader.jsx
@@ -0,0 +1,15 @@
+import { Container, Navbar } from 'react-bootstrap';
+
+function NavHeader(props) {
+    return(
+        <>
+            <Navbar bg='primary' data-bs-theme='dark'>
+                <Container fluid>
+                    <Navbar.Brand>HeapOverrun - Question #{props.questionNum}</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavHeader;
